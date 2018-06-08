87761266b79f1c4f65096a366f598cfa19e1fbed
Sergio Roselló Morell <sergio-rosello@hotmail.com>
2018-06-09 00:15:14 +0200

    Final project clean-up

--- ProyectoFinal/Assets/Scripts/LeaderboardController.cs
+++ ProyectoFinal/Assets/Scripts/LeaderboardController.cs
@@ -37,7 +37,7 @@ public class LeaderboardController : MonoBehaviour
 
     IEnumerator WaitForServer()
     {
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(10.0f);
         for (int i = 0; i < ogm.highScores.Count; i++)
         {
             leaderboardIndex.text += i + 1 + ".\n";
--- ServidorProyectoFinal/misc.py
+++ ServidorProyectoFinal/misc.py
@@ -1,5 +1,3 @@
-import math
-
 
 def split(l, size):
     """
@@ -15,29 +13,3 @@ def split(l, size):
         l = l[size:]
     arrs.append(l)
     return arrs
-
-
-def distance(x1, y1, x2, y2):
-    """
-    Calcula la distancia entre dos puntos
-    :param x1: Coordenada x del primer punto
-    :param y1: Coordenada y del primer punto
-    :param x2: Coordenada x del segundo punto
-    :param y2: Coordenada y del segundo punto
-    :return: Distancia entre los puntos
-    """
-    return math.sqrt(((x1 - x2) ** 2) + ((y1 - y2) ** 2))
-
-
-def estimate_current_position(last_position, velocity, time_delta):
-    """
-    Calcula la posicion actual del un objeto
-    :param last_position: ultima posicion recibida
-    :param velocity: Velocidad que llevaba el objeto
-    :param time_delta: Tiempo que ha pasado desde que se recibio la ultima posicion
-    :return:
-    """
-    return {
-        'x': last_position['x'] + velocity['x'] * time_delta,
-        'y': last_position['y'] + velocity['y'] * time_delta,
-    }
